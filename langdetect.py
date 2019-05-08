df7ef878029ccd1c7a33119c000e80b991f7a34e
sepkeuchenius <septimber@gmail.com>
2019-05-08 13:15:20 +0200

    Update langdetect.py

--- langdetect.py
+++ langdetect.py
@@ -7,15 +7,5 @@ Created on Wed May  8 12:10:27 2019
 
 #Testtest
 import re
-def prepare(text):
-    return print("Nope")
-<<<<<<< HEAD
-
-def prepare()
-def trigrams()
-def trigram_table()
-def read_table()
-def write_table()
-def cosine_similarity()
-=======
->>>>>>> 4025dc8ab7d5bb7c96594ef521a1308f8b186362
+def prepare(string):
+    return re.sub('[\!\?\,\"\.\(\)\<\>]', ' ', string).split()
