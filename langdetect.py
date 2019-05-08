d94041a37d0a686ed7753141a12f32b21c6c688a
Rob Pennekamp <rob.pennekamp@gmail.com>
2019-05-08 13:00:48 +0200

    Trigrams toegevoegd
    Done

--- langdetect.py
+++ langdetect.py
@@ -9,13 +9,19 @@ Created on Wed May  8 12:10:27 2019
 import re
 def prepare(text):
     return print("Nope")
-<<<<<<< HEAD
 
-def prepare()
-def trigrams()
-def trigram_table()
-def read_table()
-def write_table()
-def cosine_similarity()
-=======
->>>>>>> 4025dc8ab7d5bb7c96594ef521a1308f8b186362
+
+text = "Hallo dit is een test"
+
+def trigrams(text):
+    x = 0
+    trigram_list = []
+    while x < len(text)-2:
+        trigram_list.append(text[x]+text[x+1]+text[x+2])
+        x+=1
+    return trigram_list
+
+print(trigrams(text))
+
+    
+    
