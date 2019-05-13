de730949288f0fdac40188d5affbbfd884700e56
Julia <julia.sendula@gmail.com>
2019-05-13 17:29:08 +0200

    nee

--- write_profiles.py
+++ write_profiles.py
@@ -2,4 +2,12 @@ import re
 import collections
 
 def make_profiles(datafolder, profilefolder, size):
-    #doetiets
+    for file in datafolder:
+        languagename = file.split("-")[0]
+        encoding = file.split("-")[1]
+        if encoding == "UTF8":
+            bestandutf = open(file, encoding="utf-8")
+        if encoding == "Latin1":
+            bestandlatin = open(file, encoding="Latin1")
+        table =[size]
+        
