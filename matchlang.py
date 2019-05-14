e20a291973a54b8d5ab79170336c293136787f7c
Rob Pennekamp <rob.pennekamp@gmail.com>
2019-05-14 12:17:46 +0200

    Verbeterd

--- matchlang.py
+++ matchlang.py
@@ -4,4 +4,17 @@ Created on Tue May 14 11:37:16 2019
 
 @author: robpe
 """
+import langdetect
+import write_profiles
+import os
 
+
+
+class LangMatcher:
+    
+    def __init__(self, path):
+        self.dictionary = 
+    
+    def score(self, text, n=1, ngrams=200):
+        langdetect.trigram_table(text, ngrams)
+        
