3e5d276ec0a8c8b8c40da5786791d38cb4fc03e0
Julia <julia.sendula@gmail.com>
2019-05-16 14:30:28 +0200

    Update write_profiles.py

--- write_profiles.py
+++ write_profiles.py
@@ -1,14 +1,16 @@
 import os
 import langdetect
 
+
+#lees de bestanden, maak nieuwe bestanden aan met trigrams en frequenties
 def make_profiles(datafolder, profilefolder, size):
     files = os.listdir(datafolder) 
     for file in files:
         languagename = file.split("-")[0]
         encodering = file.split("-")[1]
-        bestand = open('training/' + file,'r' , encoding=encodering)
-        test = langdetect.trigram_table(bestand.read(), size)
-        filename = languagename + '.' + str(size) + '.txt'
-        newfile = open('trigram-models/' + filename, 'w', encoding="utf-8")
-        langdetect.write_trigrams(test, 'trigram-models/' + filename)
+        bestand = open('training/' + file,'r' , encoding=encodering)            #lees met juiste codering 
+        test = langdetect.trigram_table(bestand.read(), size)                   #maak een trigramtabel van tekst
+        filename = languagename + '.' + str(size) + '.txt'                      #geef een nieuwe bestandsnaam
+        newfile = open('trigram-models/' + filename, 'w', encoding="utf-8")     #maak een nieuw bestand aan
+        langdetect.write_trigrams(test, 'trigram-models/' + filename)           #maak een tekstbestand aan met de trigrams en frequentie
         newfile.close()
