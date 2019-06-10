ab7bf225ba504484d9db8db74761d3e027594870
sepkeuchenius <septimber@gmail.com>
2019-06-10 13:34:59 +0200

    pred en predpos

--- Opdracht 2/build_models.py
+++ Opdracht 2/build_models.py
@@ -9,18 +9,18 @@ from custom_chunker import ConsecutiveNPChunker
 import pickle
 import features
 
-tiny_sample = 1
+tiny_sample = 500
 # training = conll.chunked_sents("ned.train")  # Train with full dataset
 training = conll.chunked_sents("ned.train")[:tiny_sample] # SHORT DATASET: FOR DEMO/DEBUGGING ONLY!
 testing = conll.chunked_sents("ned.testa")
 simple_nl_NER = ConsecutiveNPChunker(features.simple_features_1, training)
 
-output = open("nl-tagger.pickle", "wb")
-pickle.dump(simple_nl_NER, output)
-output.close()
+#output = open("nl-tagger.pickle", "wb")
+#pickle.dump(simple_nl_NER, output)
+#output.close()
 
 
-simple_nl_NER2 = ConsecutiveNPChunker(features.simple_features_1, training, 'GIS')
-print(simple_nl_NER2.evaluate(testing))
+#simple_nl_NER2 = ConsecutiveNPChunker(features.simple_features_1, training, 'GIS')
+#print(simple_nl_NER2.evaluate(testing))
 
 print(simple_nl_NER.evaluate(testing))
