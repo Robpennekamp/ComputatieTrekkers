f8077e61a4042ccb263cd3715db9416691cd1909
Rob Pennekamp <rob.pennekamp@gmail.com>
2019-06-10 13:01:39 +0200

    kutzooi

--- Opdracht 2/build_models.py
+++ Opdracht 2/build_models.py
@@ -9,7 +9,7 @@ from custom_chunker import ConsecutiveNPChunker
 import features 
 import pickle
 
-tiny_sample = 2
+tiny_sample = 50
 # training = conll.chunked_sents("ned.train")  # Train with full dataset
 training = conll.chunked_sents("ned.train")[:tiny_sample] # SHORT DATASET: FOR DEMO/DEBUGGING ONLY! 
 testing = conll.chunked_sents("ned.testa")
@@ -17,3 +17,8 @@ simple_nl_NER = ConsecutiveNPChunker(features.simple_features_1, training)
 output = open("nl-tagger.pickle", "wb")
 pickle.dump(simple_nl_NER, output)
 output.close()
+
+simple_nl_NER2 = ConsecutiveNPChunker(features.simple_features_1, training, 'GIS')
+print(simple_nl_NER2.evaluate(testing))
+
+print(simple_nl_NER.evaluate(testing))
---
+++ Opdracht 2/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 12:42:03 2019
+
+@author: robpe
+"""
+
+import nltk
+print(nltk.classify.MaxentClassifier.ALGORITHMS)
