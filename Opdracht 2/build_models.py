d30ecafd4dd90379746196bafd7462b22b155707
sepkeuchenius <septimber@gmail.com>
2019-06-10 13:00:08 +0200

    features in one function

--- Opdracht 2/build_models.py
+++ Opdracht 2/build_models.py
@@ -6,11 +6,11 @@ Created on Fri Jun  7 11:27:05 2019
 """
 from nltk.corpus import conll2002 as conll
 from custom_chunker import ConsecutiveNPChunker
-import features 
+import features
 
-tiny_sample = 2
+tiny_sample = 50
 # training = conll.chunked_sents("ned.train")  # Train with full dataset
-training = conll.chunked_sents("ned.train")[:tiny_sample] # SHORT DATASET: FOR DEMO/DEBUGGING ONLY! 
+training = conll.chunked_sents("ned.train")[:tiny_sample] # SHORT DATASET: FOR DEMO/DEBUGGING ONLY!
 testing = conll.chunked_sents("ned.testa")
 
 simple_nl_NER = ConsecutiveNPChunker(features.simple_features_1, training)
