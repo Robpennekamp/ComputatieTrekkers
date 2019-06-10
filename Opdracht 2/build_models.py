023af62bdbccb4e94ac494049d1720d8187092ca
Rob Pennekamp <rob.pennekamp@gmail.com>
2019-06-10 14:57:05 +0200

    Goeie Custom Chunker

--- Opdracht 2/build_models.py
+++ Opdracht 2/build_models.py
@@ -9,15 +9,15 @@ from custom_chunker import ConsecutiveNPChunker
 import pickle
 import features
 
-tiny_sample = 50
+tiny_sample = 10000
 # training = conll.chunked_sents("ned.train")  # Train with full dataset
-training = conll.chunked_sents("ned.train")[:tiny_sample] # SHORT DATASET: FOR DEMO/DEBUGGING ONLY!
+training = conll.chunked_sents("ned.train") # SHORT DATASET: FOR DEMO/DEBUGGING ONLY!
 testing = conll.chunked_sents("ned.testa")
-simple_nl_NER = ConsecutiveNPChunker(features.simple_features_2, training, 'GIS')
+simple_nl_NER = ConsecutiveNPChunker(features.simple_features_2, training, 'NaiveBayes')
 
-#output = open("nl-tagger.pickle", "wb")
-#pickle.dump(simple_nl_NER, output)
-#output.close()
+output = open("nl-tagger.pickle", "wb")
+pickle.dump(simple_nl_NER, output)
+output.close()
 
 
 #simple_nl_NER2 = ConsecutiveNPChunker(features.simple_features_1, training, 'GIS')
